12eaba6ae2962d402340a5385c7e42fd1cb20e0d
Brian Cobb <bcobb@uwalumni.com>
2017-12-07 07:42:36 -0500

    Day 2, part 2

--- src/advent/two.clj
+++ src/advent/two.clj
@@ -1,5 +1,6 @@
 (ns advent.two
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as c]))
 
 (defn line-to-numbers [line]
   (map (fn [char] (Integer/parseInt (str char)))
@@ -11,13 +12,33 @@
 (defn read-input []
   (read-document (slurp "resources/two.txt")))
 
-(defn checksum-part [row]
+(defn min-plus-max [row]
   (let [upper (apply max row)
         lower (apply min row)]
     (- upper lower)))
 
-(defn checksum [rows]
-  (reduce + (map checksum-part rows)))
+(defn divisible? [a b]
+  (or
+   (zero? (mod a b))
+   (zero? (mod b a))))
+
+(defn quotient [a b]
+  (let [numerator (max a b)
+        denominator (min a b)]
+    (/ numerator denominator)))
+
+(defn quotient-of-divisible-items [row]
+  (let [combos (c/combinations row 2)
+        candidates (filter (partial apply divisible?) combos)]
+    (assert (= 1 (count candidates)))
+    (apply quotient (first candidates))))
+
+(defn checksum
+  [rows row-checksum-fn]
+  (reduce + (map row-checksum-fn rows)))
 
 (defn run []
-  (checksum (read-input)))
+  (checksum (read-input) min-plus-max))
+
+(defn run-again []
+  (checksum (read-input) quotient-of-divisible-items))

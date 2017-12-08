f1f574197e15d093a74b45f427c2c3d10c8ec2b2
Brian Cobb <bcobb@uwalumni.com>
2017-12-08 08:10:44 -0500

    Day 5, part 1 refactor

--- src/advent/five.clj
+++ src/advent/five.clj
@@ -10,15 +10,19 @@
   (or (neg? position)
       (<= (count offsets) position)))
 
-(defn new-offsets [offsets last-position]
+(defn simple-offset [offsets last-position]
   (update offsets last-position inc))
 
-(defn move-once [{position :position
-                  offsets :offsets}]
-  (let [jump (nth offsets position)
-        new-position (+ position jump)]
-    {:offsets (new-offsets offsets position)
-     :position new-position}))
+(defn move-once
+  ([state]
+   (move-once state simple-offset))
+  ([{position :position
+     offsets :offsets}
+    new-offsets-fn]
+   (let [jump (nth offsets position)
+         new-position (+ position jump)]
+     {:offsets (new-offsets-fn offsets position)
+      :position new-position})))
 
 (defn steps-to-exit [offsets]
   (loop [n 0

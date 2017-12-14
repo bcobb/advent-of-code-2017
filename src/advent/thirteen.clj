3e1be9df727f6341d3744a576ab25d9669581c8c
Brian Cobb <bcobb@uwalumni.com>
2017-12-14 08:10:12 -0500

    Day 13, part 1

---
+++ src/advent/thirteen.clj
@@ -0,0 +1,57 @@
+(ns advent.thirteen
+  (:require [clojure.string :as str]))
+
+(defn update-firewall [firewall line]
+  (let [[depth range] (map #(Integer/parseInt %) (str/split line #": "))]
+    (assoc firewall depth range)))
+
+(defn max-depth [firewall]
+  (apply max (keys firewall)))
+
+(defn min-depth [firewall]
+  (apply min (keys firewall)))
+
+(defn depths [firewall]
+  (range (min-depth firewall)
+         (inc (max-depth firewall))))
+
+(defn range-at [firewall depth]
+  (get firewall depth 0))
+
+(defn oscillate-to [n]
+  (if (zero? n)
+    (repeat 0)
+    (letfn [(oscillator
+              ([] (oscillator 0 1))
+              ([a b]
+               (let [step (if (or (< a b n) (zero? b)) inc dec)]
+                 (lazy-seq (cons a (oscillator b (step b)))))))]
+      (oscillator))))
+
+(defn scanner-position [firewall depth t]
+  (let [max-index (dec (range-at firewall depth))
+        oscillator (oscillate-to max-index)]
+    (last (take (inc t) oscillator))))
+
+(defn caught-at? [firewall depth t]
+  (when (get firewall depth)
+    (zero? (scanner-position firewall depth t))))
+
+(defn severity [firewall depth t]
+  (if (caught-at? firewall depth t)
+    (* depth (range-at firewall depth))
+    0))
+
+(defn parse-input [input]
+  (let [lines (str/split input #"\n")
+        firewall (reduce update-firewall {} lines)]
+    firewall))
+
+(defn get-firewall []
+  (parse-input (slurp "resources/thirteen.txt")))
+
+(defn run []
+  (let [firewall (get-firewall)]
+    (apply + (map (partial severity firewall)
+                  (depths firewall)
+                  (depths firewall)))))

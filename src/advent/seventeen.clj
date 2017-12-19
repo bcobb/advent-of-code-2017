dc337d7c6629760cbe7119b286385ef6e17cc092
Brian Cobb <bcobb@uwalumni.com>
2017-12-19 07:53:19 -0500

    Day 17, part 2

--- src/advent/seventeen.clj
+++ src/advent/seventeen.clj
@@ -7,11 +7,31 @@
    :next 1
    :steps steps})
 
+(defn make-mini-state []
+  {:length 1
+   :inserted 0
+   :position 0})
+
 (defn step [{buffer :buffer
              steps :steps :as state}]
   (let [length (count buffer)]
     (update state :position #(mod (+ steps %) length))))
 
+(defn mini-stepper [steps]
+  (fn [{length :length :as state}]
+    (-> state
+        (update :position #(mod (+ steps %) length))
+        (update :position inc)
+        (update :inserted inc)
+        (update :length inc))))
+
+(defn at-position? [n]
+  (fn [{position :position}]
+    (= position n)))
+
+(defn not-inserted? [n]
+  (fn [{inserted :inserted}]
+    (<= inserted n)))
 
 (defn insert [v index value]
   (let [before (subvec v 0 (inc index))
@@ -43,3 +63,9 @@
 
 (defn run []
   (next-value (last (take 2018 (iterate evolve (make-state 356))))))
+
+(defn run-again []
+  (let [steps (iterate (mini-stepper 356) (make-mini-state))
+        relevant-steps (take-while (not-inserted? 50000000) steps)
+        insertions-after-zero (filter (at-position? 1) relevant-steps)]
+    (:inserted (last insertions-after-zero))))

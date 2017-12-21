284296033bf27a45a1e674d719705fb8f161db65
Brian Cobb <bcobb@uwalumni.com>
2017-12-21 09:31:14 -0600

    Day 18, part 2

--- src/advent/eighteen.clj
+++ src/advent/eighteen.clj
@@ -12,7 +12,9 @@
 (def identity-or-zero (fnil identity 0))
 
 (defn registry-get [state key]
-  (get-in state [:registry key] 0))
+  (if (string? key)
+    (get-in state [:registry key] 0)
+    key))
 
 (defn move-forward [state]
   (update state :position inc))
@@ -23,6 +25,15 @@
 (defn update-register [state register & args]
   (let [state' (ensure-register state register)]
     (apply update-in state' [:registry register] args)))
+
+(defn read-inbox-once [state]
+  (update state :inbox (comp vec rest)))
+
+(defn add-to-outbox [state message]
+  (update state :outbox conj message))
+
+(defn increment-sent [state]
+  (update state :sent-messages inc))
 
 (defn argument [i x]
   (if (zero? i)
@@ -40,14 +51,6 @@
         args (map-indexed argument (rest tokens))]
     {:name name
      :args args}))
-
-(defn i:snd [a]
-  (fn [state]
-    (let [frequency (registry-get state a)]
-      (-> state
-          move-forward
-          (ensure-register a)
-          (update :played conj frequency)))))
 
 (defn i:set [a v]
   (fn [state]
@@ -75,14 +78,24 @@
         (update-register a #(mod % (b state))))))
 
 (defn i:rcv [a]
-  (fn [{played :played :as state}]
-    (let [val (registry-get state a)
-          last-played (last played)]
-      (if (not (zero? val))
-        (-> state
-            move-forward
-            (update :recovered conj last-played))
-        (move-forward state)))))
+  (fn [state]
+    (let [inbox (get state :inbox)]
+      (if (empty? inbox)
+        (assoc state :state :waiting)
+        (let [message (first inbox)]
+          (-> state
+              (assoc :state :running)
+              move-forward
+              read-inbox-once
+              (update-register a (constantly message))))))))
+
+(defn i:snd [a]
+  (fn [state]
+    (let [message (registry-get state a)]
+      (-> state
+          move-forward
+          increment-sent
+          (add-to-outbox message)))))
 
 (defn i:jgz [a v]
   (fn [state]
@@ -106,14 +119,17 @@
   (let [f (get instruction->fn name ::no-function)]
     (apply f args)))
 
+(defn lines->instructions [lines]
+  (mapv
+   (comp parse-instruction line->instruction)
+   lines))
+
 (defn evaluate-program [lines]
-  (let [instructions (mapv
-                      (comp parse-instruction line->instruction)
-                      lines)]
+  (let [instructions (lines->instructions lines)]
     (loop [state (initial-state)
            position (get state :position)
            instruction (nth instructions position)]
-      (if (not-empty (get state :recovered))
+      (if (nil? instruction)
         state
         (let [next-state (instruction state)
               next-position (get next-state :position)
@@ -121,3 +137,60 @@
           (recur next-state
                  next-position
                  next-instruction))))))
+
+(defn make-program [number]
+  {:id number
+   :position 0
+   :registry {"p" number}
+   :state :running
+   :sent-messages 0
+   :inbox []
+   :outbox []})
+
+(defn evaluation-context [instructions]
+  {:instructions instructions
+   :programs {0 (make-program 0)
+              1 (make-program 1)}})
+
+(defn dispatch-message [context sender-entry]
+  (let [sender-id (key sender-entry)
+        sender (val sender-entry)
+        messages (get sender :outbox)
+        programs (get context :programs)
+        recipient (-> programs
+                       (dissoc sender-id)
+                       vals
+                       first)
+        recipient-id (get recipient :id)]
+    (-> context
+        (assoc-in [:programs sender-id :outbox] [])
+        (update-in [:programs recipient-id :inbox] into messages))))
+
+(defn dispatch-messages [context]
+  (let [programs (get context :programs)]
+    (reduce dispatch-message context programs)))
+
+(defn evaluate-next-instruction [context program-entry]
+  (let [program-id (key program-entry)
+        program (val program-entry)
+        position (get program :position)
+        instructions (get context :instructions)
+        instruction (nth instructions position)]
+    (-> context
+        (update-in [:programs program-id] instruction)
+        dispatch-messages)))
+
+(defn deadlocked? [context]
+  (let [all-waiting? (every? (partial = :waiting) (map (comp :state val) (get context :programs)))
+        inboxes-empty? (every? empty? (map (comp :inbox val) (get context :programs)))]
+    (and all-waiting? inboxes-empty?)))
+
+(defn run-again []
+  (let [instructions (lines->instructions (read-input))
+        initial-context (evaluation-context instructions)]
+    (loop [context initial-context]
+      (if (deadlocked? context)
+        (get-in context [:programs 1 :sent-messages])
+        (let [programs (get context :programs)
+              new-context (reduce evaluate-next-instruction context programs)]
+          (recur new-context))))))

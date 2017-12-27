19a592c0cd3c8d1d77f09fc2df3d947835157d89
Brian Cobb <bcobb@uwalumni.com>
2017-12-27 10:11:50 -0500

    Day 21, part 1 & 2

--- src/advent/twenty_one.clj
+++ src/advent/twenty_one.clj
@@ -1,7 +1,16 @@
 (ns advent.twenty-one
   (:require [clojure.string :as str]
             [clojure.set :as set]
-            [clojure.math.combinatorics :as combo]))
+            [clojure.math.combinatorics :as combo]
+            [clojure.zip :as zip]))
+
+(defn flat-map [f coll]
+  (reduce (fn [coll' el] (apply conj coll' (f el)))
+          []
+          coll))
+
+(defn partitionv [n coll]
+  (mapv vec (partition n coll)))
 
 (defn pattern-to-parts [pattern]
   (str/split pattern #" => "))
@@ -29,25 +38,29 @@
 
 (def plane->matrix-2 (set/map-invert matrix-2->plane))
 
-(defn project-3 [coords]
-  (mapv matrix-3->plane coords))
+(defmulti project (fn [matrix coords] (count matrix)))
 
-(defn project-2 [coords]
+(defmethod project 2 [_ coords]
   (mapv matrix-2->plane coords))
 
-(defn unproject-3 [coords]
-  (mapv plane->matrix-3 coords))
+(defmethod project 3 [_ coords]
+  (mapv matrix-3->plane coords))
 
-(defn unproject-2 [coords]
+(defmulti unproject (fn [matrix coords] (count matrix)))
+
+(defmethod unproject 2 [_ coords]
   (mapv plane->matrix-2 coords))
+
+(defmethod unproject 3 [_ coords]
+  (mapv plane->matrix-3 coords))
 
 (defn rotate-projected [[x y]]
   [y (* -1 x)])
 
-(defn flip-horizontal-projected-3 [[x y]]
+(defn flip-horizontal-projected [[x y]]
   [x (* -1 y)])
 
-(defn flip-vertical-projected-3 [[x y]]
+(defn flip-vertical-projected [[x y]]
   [(* -1 x) y])
 
 (defn all-coordinates [matrix]
@@ -56,30 +69,26 @@
 
 (defn matrix-transformer [xform]
   (fn [matrix]
-    (let [coordinates (all-coordinates matrix)
+    (let [project* (partial project matrix)
+          unproject* (partial unproject matrix)
+          coordinates (all-coordinates matrix)
           pixels (map (partial get-in matrix) coordinates)
-          transform (comp unproject-3 xform project-3)
+          transform (comp unproject* xform project*)
           new-coordinates (map transform coordinates)
           transforms (map vector new-coordinates pixels)]
       (reduce (partial apply assoc-in) matrix transforms))))
 
-(def rotate-3 (matrix-transformer rotate-projected-3))
-(def flip-horizontal-3 (matrix-transformer flip-horizontal-projected-3))
-(def flip-vertical-3 (matrix-transformer flip-vertical-projected-3))
-
-(defn translate-vertical [n [x y]]
-  [(+ x n) y])
-
-(defn translate-horizontal [n [x y]]
-  [x (+ y n)])
+(def rotate (matrix-transformer rotate-projected))
+(def flip-horizontal (matrix-transformer flip-horizontal-projected))
+(def flip-vertical (matrix-transformer flip-vertical-projected))
 
 (defn inspect-matrix [matrix]
   (str/join "\n" (map #(str/join " " %) matrix)))
 
 (defn matrix-permutations [matrix]
-  (let [rotations (take 4 (iterate rotate-3 matrix))
-        horizontal-flips (map flip-horizontal-3 rotations)
-        vertical-flips (map flip-vertical-3 rotations)]
+  (let [rotations (take 4 (iterate rotate matrix))
+        horizontal-flips (map flip-horizontal rotations)
+        vertical-flips (map flip-vertical rotations)]
     (set (concat rotations horizontal-flips vertical-flips))))
 
 (def initial-matrix (lines-to-grids (part-to-lines ".#./..#/###")))
@@ -90,7 +99,67 @@
         enhancements (map last basis)]
     (zipmap canonical-patterns enhancements)))
 
+(defn canonical-pattern [pattern-library matrix]
+  (let [options (matrix-permutations matrix)]
+    (first (filter (partial get pattern-library) options))))
+
+(def canonical (partial canonical-pattern (read-pattern-library)))
+
 (defn enhancement [pattern-library matrix]
-  (let [options (matrix-permutations matrix)
-        canonical-pattern (first (filter (partial get pattern-library) options))]
-    (get pattern-library canonical-pattern)))
+  (get pattern-library (canonical-pattern pattern-library matrix)))
+
+(def enhance-submatrix (partial enhancement (read-pattern-library)))
+
+;; takes a matrix, splits each row into vectors of size n
+(defn split-rows-to-size [n matrix]
+  (mapv #(partitionv n %) matrix))
+
+;; takes a matrix, returns a matrix with an additional layer of nesting around each n row vectors
+;; matrix* implies that this generally takes a matrix that's had its row structure modified
+(defn group-rows-to-size [n matrix*]
+  (partitionv n matrix*))
+
+;; takes a matrix which has had its rows split to size and its row vectors
+;; grouped so that the first elements of each group together form a submatrix,
+;; as do the second, third, etc elements
+(defn collect-submatrices-of-size [n matrix*]
+  (mapv #(partitionv n (apply interleave %)) matrix*))
+
+(defn detect-submatrices-of-size [submatrix-size matrix]
+  (->> matrix
+       (split-rows-to-size submatrix-size)
+       (group-rows-to-size submatrix-size)
+       (collect-submatrices-of-size submatrix-size)
+       (flat-map identity)))
+
+;; order is important - only find submatrices of size 3 if it would be impossible to find submatrices of size 2
+(defmulti submatrices (fn [matrix] (if (zero? (mod (count matrix) 2))
+                                     2
+                                     3)))
+
+(defmethod submatrices 3 [matrix]
+  (detect-submatrices-of-size 3 matrix))
+
+(defmethod submatrices 2 [matrix]
+  (detect-submatrices-of-size 2 matrix))
+
+;; enhances each submatrix of the given matrix, and returns the whole
+;; enhanced matrix by some dark, dark magic
+(defn enhance [matrix]
+  (let [submatrices (submatrices matrix)
+        enhanced-submatrices (mapv enhance-submatrix submatrices)
+        side-length (int (Math/sqrt (count submatrices)))]
+    (->> enhanced-submatrices
+         (partitionv side-length)
+         (mapv (partial apply interleave))
+         (flat-map (partial partitionv side-length))
+         (mapv (comp vec flatten)))))
+
+(defn iterate-to [n]
+  (last (take (inc n) (iterate enhance initial-matrix))))
+
+(defn on? [e]
+  (= \# e))
+
+(defn on-after [n]
+  (count (filter on? (flatten (iterate-to n)))))

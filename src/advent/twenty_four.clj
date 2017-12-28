a66ffe28386a4e95d7f34ef96005af4ca385b7c7
Brian Cobb <bcobb@uwalumni.com>
2017-12-28 09:22:11 -0500

    Day 24, part 2

--- src/advent/twenty_four.clj
+++ src/advent/twenty_four.clj
@@ -16,6 +16,7 @@
 (defn initial-bridge []
   {:terminal 0
    :strength 0
+   :length 0
    :finished? false
    :catalog components-catalog})
 
@@ -32,15 +33,17 @@
 (defn use-connector [catalog [lhs rhs :as component]]
   (disj catalog component))
 
-(defn augment-bridge [{:keys [strength catalog terminal] :as bridge} component]
+(defn augment-bridge [{:keys [strength length catalog terminal] :as bridge} component]
   (let [new-catalog (use-connector catalog component)
         [lhs rhs] component
         new-strength (apply + strength component)
-        new-terminal (if (= lhs terminal) rhs lhs)]
+        new-terminal (if (= lhs terminal) rhs lhs)
+        new-length (inc length)]
     (assoc bridge
            :terminal new-terminal
            :last-used component
            :catalog new-catalog
+           :length new-length
            :strength new-strength)))
 
 (defn subsequent-bridges [bridges]
@@ -62,3 +65,10 @@
 
 (defn run []
   (:strength (last (sort-by :strength (build-bridges)))))
+
+(defn run-again []
+  (let [bridges (build-bridges)
+        greatest-length (apply max (map :length bridges))
+        longest-bridges (filter #(= greatest-length (:length %)) bridges)
+        strongest-of-the-longest (last (sort-by :strength longest-bridges))]
+    (:strength strongest-of-the-longest)))
